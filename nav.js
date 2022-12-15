2ac7d66511ef3d2fcb393c812beaae4f37be94b9
lowLevelGod <petrepopa19@gmail.com>
2022-12-15 19:32:34 +0200

    layout done

--- nav.js
+++ nav.js
@@ -1,14 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
+window.addEventListener('DOMContentLoaded', function() {
+
+    // Look for .hamburger
+  const hamburger = document.querySelector(".hamburger");
+  const navLinksWrapper = document.querySelector(".nav-links-wrapper");
+  const navLinks = document.querySelectorAll(".nav-links-wrapper li");
+  hamburger.classList.remove("is-active");
+  navLinksWrapper.classList.remove("show-navLinksWrapper");
+console.log("Hello");
+  // On click
+  hamburger.addEventListener("click", function () {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    navLinksWrapper.classList.toggle("show-navLinksWrapper");
+    const navLinks_array = [...navLinks];
+    navLinks_array.forEach((i) => {
+      i.classList.toggle("show-nav-link");
     });
-
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+  });
 });
